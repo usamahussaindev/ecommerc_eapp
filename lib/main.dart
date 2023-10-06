bc41f5875487297a82a3208b32d5f18d062ac1df
usamahussaindev <usamahussaindev@gmail.com>
2023-10-06 12:16:34 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:ecommerc_eapp/pages/auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -17,11 +17,12 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      initialRoute: "/AuthPage",
+      initialRoute: "/login",
       routes: {
         "/": (context) => AuthPage(),
         "/home": (context) => HomePage(),
--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [Image.asset("assets/images/auth.jpg")],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/auth.jpg",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Passsword",
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("App working fine!");
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom())
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
